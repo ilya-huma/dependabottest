517f0dfe3e942ecf70dcb7978b1c931b0d500598
ilya <ilya.kulikov@huma.com>
2021-07-07 16:34:23 +0300

    renovate update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
     implementation("androidx.core:core-ktx:1.6.0")
+    implementation(Deps.AndroidX.navFragment)
     implementation(Deps.AndroidX.appCompat)
     implementation("com.google.android.material:material:1.4.0")
     testImplementation("junit:junit:4.13.2")
