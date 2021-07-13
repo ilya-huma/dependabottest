da7409f173d7e720cf463dbba8424ac6ee8fc653
ilya <ilya.kulikov@huma.com>
2021-07-14 01:30:53 +0300

    test commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
     implementation("androidx.core:core-ktx:1.6.0")
-    runtimeOnly(libs.appCompat)
-    runtimeOnly(libs.recycler)
+    implementation(libs.appCompat)
+    implementation(libs.recycler)
     implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
     implementation("com.google.android.material:material:1.4.0")
     testImplementation("junit:junit:4.13.2")
